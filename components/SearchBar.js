101baec069126c3f275289b6036d01de2303388c
yfoucade <yohan.foucade@gmail.com>
2024-02-21 16:21:39 +0100

    ux: add option to clear form

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -20,12 +20,22 @@ app.component('search-bar', {
                 <option value="parti_ratt_financier">Parti</option>
             </select>
             <input type="text" placeholder="Chercher un·e député·e" v-model="search_string" @input="onChange">
+            <span class="clear" @click="clearForm">
+                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="grey" class="bi bi-x-lg" viewBox="0 0 20 20">
+                    <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
+                </svg>
+            </span>
         </div>
     </div>
     `,
 
     methods: {
         onChange() {
+            this.$emit('search-string-updated', {search_criterion: this.search_criterion, search_string: this.search_string});
+        },
+        clearForm() {
+            this.search_criterion = "";
+            this.search_string = "";
             this.$emit('search-string-updated', {search_criterion: this.search_criterion, search_string: this.search_string});
         }
     }
