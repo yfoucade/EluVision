cd348a13e55bf9a39e92faa9bce7f1cdb2984ef5
yfoucade <yohan.foucade@gmail.com>
2024-02-23 14:01:43 +0100

    minor change: indentation

--- src/eluvision-app/src/App.vue
+++ src/eluvision-app/src/App.vue
@@ -21,24 +21,19 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   padding-bottom: 150px;
-  /* font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif; */
-}
-
-body {
-    /* font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif; */
-    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
 }
 
 ::-webkit-scrollbar {
-    display: none;
+  display: none;
 }
 
 a {
-    text-decoration: none !important;
-    color: black !important;
+  text-decoration: none !important;
+  color: black !important;
 }
 
 .back-button {
-    margin-top: 30px;
+  margin-top: 30px;
 }
 </style>
