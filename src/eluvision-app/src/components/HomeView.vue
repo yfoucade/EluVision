60fd52d2363038c06f6f86f0fb979e5611e487da
yfoucade <yohan.foucade@gmail.com>
2024-02-23 14:03:07 +0100

    refactor: create component for table headers

--- src/eluvision-app/src/components/HomeView.vue
+++ src/eluvision-app/src/components/HomeView.vue
@@ -1,70 +1,18 @@
 <template>
   <search-bar @search-string-updated="updateSearchString"></search-bar>
-  <div id="results-list" v-if="all_deputies && state == 'list'">
+  <div id="results-list" v-if="all_deputies">
+
     <div class="results-found" v-if="filtered_list.length">
       <table>
+
         <tr>
-          <th @click="updateSortBy('nom_de_famille')">
-            <span>Nom</span>
-            <span class="sort-icon" v-if="sort_by == 'nom_de_famille'">
-              <svg v-if="ascending" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="black"
-                class="bi bi-sort-down-alt" viewBox="0 0 16 16">
-                <path
-                  d="M3.5 3.5a.5.5 0 0 0-1 0v8.793l-1.146-1.147a.5.5 0 0 0-.708.708l2 1.999.007.007a.497.497 0 0 0 .7-.006l2-2a.5.5 0 0 0-.707-.708L3.5 12.293zm4 .5a.5.5 0 0 1 0-1h1a.5.5 0 0 1 0 1zm0 3a.5.5 0 0 1 0-1h3a.5.5 0 0 1 0 1zm0 3a.5.5 0 0 1 0-1h5a.5.5 0 0 1 0 1zM7 12.5a.5.5 0 0 0 .5.5h7a.5.5 0 0 0 0-1h-7a.5.5 0 0 0-.5.5" />
-              </svg>
-              <svg v-else xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="black" class="bi bi-sort-up"
-                viewBox="0 0 16 16">
-                <path
-                  d="M3.5 12.5a.5.5 0 0 1-1 0V3.707L1.354 4.854a.5.5 0 1 1-.708-.708l2-1.999.007-.007a.5.5 0 0 1 .7.006l2 2a.5.5 0 1 1-.707.708L3.5 3.707zm3.5-9a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5M7.5 6a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1zm0 3a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1zm0 3a.5.5 0 0 0 0 1h1a.5.5 0 0 0 0-1z" />
-              </svg>
-            </span>
-          </th>
-          <th @click="updateSortBy('prenom')">
-            <span>Prénom</span>
-            <span class="sort-icon" v-if="sort_by == 'prenom'">
-              <svg v-if="ascending" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="black"
-                class="bi bi-sort-down-alt" viewBox="0 0 16 16">
-                <path
-                  d="M3.5 3.5a.5.5 0 0 0-1 0v8.793l-1.146-1.147a.5.5 0 0 0-.708.708l2 1.999.007.007a.497.497 0 0 0 .7-.006l2-2a.5.5 0 0 0-.707-.708L3.5 12.293zm4 .5a.5.5 0 0 1 0-1h1a.5.5 0 0 1 0 1zm0 3a.5.5 0 0 1 0-1h3a.5.5 0 0 1 0 1zm0 3a.5.5 0 0 1 0-1h5a.5.5 0 0 1 0 1zM7 12.5a.5.5 0 0 0 .5.5h7a.5.5 0 0 0 0-1h-7a.5.5 0 0 0-.5.5" />
-              </svg>
-              <svg v-else xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="black" class="bi bi-sort-up"
-                viewBox="0 0 16 16">
-                <path
-                  d="M3.5 12.5a.5.5 0 0 1-1 0V3.707L1.354 4.854a.5.5 0 1 1-.708-.708l2-1.999.007-.007a.5.5 0 0 1 .7.006l2 2a.5.5 0 1 1-.707.708L3.5 3.707zm3.5-9a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5M7.5 6a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1zm0 3a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1zm0 3a.5.5 0 0 0 0 1h1a.5.5 0 0 0 0-1z" />
-              </svg>
-            </span>
-          </th>
-          <th @click="updateSortBy('nom_circo')">
-            <span>Département</span>
-            <span class="sort-icon" v-if="sort_by == 'nom_circo'">
-              <svg v-if="ascending" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="black"
-                class="bi bi-sort-down-alt" viewBox="0 0 16 16">
-                <path
-                  d="M3.5 3.5a.5.5 0 0 0-1 0v8.793l-1.146-1.147a.5.5 0 0 0-.708.708l2 1.999.007.007a.497.497 0 0 0 .7-.006l2-2a.5.5 0 0 0-.707-.708L3.5 12.293zm4 .5a.5.5 0 0 1 0-1h1a.5.5 0 0 1 0 1zm0 3a.5.5 0 0 1 0-1h3a.5.5 0 0 1 0 1zm0 3a.5.5 0 0 1 0-1h5a.5.5 0 0 1 0 1zM7 12.5a.5.5 0 0 0 .5.5h7a.5.5 0 0 0 0-1h-7a.5.5 0 0 0-.5.5" />
-              </svg>
-              <svg v-else xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="black" class="bi bi-sort-up"
-                viewBox="0 0 16 16">
-                <path
-                  d="M3.5 12.5a.5.5 0 0 1-1 0V3.707L1.354 4.854a.5.5 0 1 1-.708-.708l2-1.999.007-.007a.5.5 0 0 1 .7.006l2 2a.5.5 0 1 1-.707.708L3.5 3.707zm3.5-9a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5M7.5 6a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1zm0 3a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1zm0 3a.5.5 0 0 0 0 1h1a.5.5 0 0 0 0-1z" />
-              </svg>
-            </span>
-          </th>
-          <th @click="updateSortBy('groupe_sigle')">
-            <span>Groupe</span>
-            <span class="sort-icon" v-if="sort_by == 'groupe_sigle'">
-              <svg v-if="ascending" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="black"
-                class="bi bi-sort-down-alt" viewBox="0 0 16 16">
-                <path
-                  d="M3.5 3.5a.5.5 0 0 0-1 0v8.793l-1.146-1.147a.5.5 0 0 0-.708.708l2 1.999.007.007a.497.497 0 0 0 .7-.006l2-2a.5.5 0 0 0-.707-.708L3.5 12.293zm4 .5a.5.5 0 0 1 0-1h1a.5.5 0 0 1 0 1zm0 3a.5.5 0 0 1 0-1h3a.5.5 0 0 1 0 1zm0 3a.5.5 0 0 1 0-1h5a.5.5 0 0 1 0 1zM7 12.5a.5.5 0 0 0 .5.5h7a.5.5 0 0 0 0-1h-7a.5.5 0 0 0-.5.5" />
-              </svg>
-              <svg v-else xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="black" class="bi bi-sort-up"
-                viewBox="0 0 16 16">
-                <path
-                  d="M3.5 12.5a.5.5 0 0 1-1 0V3.707L1.354 4.854a.5.5 0 1 1-.708-.708l2-1.999.007-.007a.5.5 0 0 1 .7.006l2 2a.5.5 0 1 1-.707.708L3.5 3.707zm3.5-9a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5M7.5 6a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1zm0 3a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1zm0 3a.5.5 0 0 0 0 1h1a.5.5 0 0 0 0-1z" />
-              </svg>
-            </span>
+          <th v-for="(header, index) in table_headers" :key="index" @click="updateSortBy(header.target_attribute)">
+            <table-header-component :colname="header.colname"
+              :sort="sort_by == header.target_attribute ? (ascending ? 'ascending' : 'descending') : 'no'">
+            </table-header-component>
           </th>
         </tr>
+
         <tr class="deputy-row" v-for="(deputy, index) in filtered_list" :key="index" @click="showDetails(index)"
           @mouseover="showDeputyTag(index)" @mouseleave="hideDeputyTag">
           <td>{{ deputy.nom_de_famille }}</td>
@@ -72,46 +20,56 @@
           <td>{{ deputy.nom_circo }}</td>
           <td>{{ deputy.groupe_sigle }}</td>
         </tr>
+
       </table>
+
       <div class="deputy-tag" v-if="show_deputy_tag && mousePosX"
         :style="{ position: 'absolute', top: `${mousePosY + 3}px`, left: `${mousePosX}px`, }">
         <img :src="deputy_tag_img">
       </div>
     </div>
+
     <p v-else>Aucun résultat ne correspond à votre recherche</p>
+
   </div>
 </template>
 
 <script>
 import SearchBar from "./SearchBar.vue";
+import TableHeaderComponent from "./TableHeaderComponent.vue";
 
 export default {
   data() {
     return {
       all_deputies: null,
       sort_by: 'nom_de_famille',
-      current_deputy: null,
       ascending: true,
+      current_deputy: null,
+      show_deputy_tag: false,
       deputy_tag_img: "",
       mousePosX: 0,
       mousePosY: 0,
       search_criterion: null,
       search_string: "",
-      show_deputy_tag: false,
-      state: 'list',
+      table_headers: [
+        { colname: "Nom", target_attribute: "nom_de_famille" },
+        { colname: "Prénom", target_attribute: "prenom" },
+        { colname: "Département", target_attribute: "nom_circo" },
+        { colname: "Groupe", target_attribute: "groupe_sigle" },
+      ]
     }
   },
 
   computed: {
     filtered_list() {
-      let res = [...this.all_deputies];
+      let res = [ ...this.all_deputies ];
       if (this.search_string)
         res = this.all_deputies.filter((deputy_obj) => this.deputyMatchesSearch(deputy_obj, this.search_string));
       return res.sort((a, b) => (2 * this.ascending - 1) * a[this.sort_by].localeCompare(b[this.sort_by]));
     }
   },
 
-  components: { SearchBar, },
+  components: { SearchBar, TableHeaderComponent, },
 
   methods: {
     async init_deputy_list() {
@@ -134,11 +92,6 @@ export default {
       this.$router.push({ path: `/details/${slug}` });
     },
 
-    showList() {
-      this.state = 'list';
-      this.hideDeputyTag();
-    },
-
     updateSearchString(criterion_and_string) {
       this.search_criterion = criterion_and_string.search_criterion;
       if (criterion_and_string.search_string)
@@ -149,27 +102,12 @@ export default {
     },
 
     deputyMatchesSearch(deputy_obj, search_string) {
+      // Use criterion if there is one, otherwise, search default attributes
       for (let colname of (this.search_criterion ? [this.search_criterion] : ['nom_de_famille', 'prenom', 'nom_circo', 'num_deptmt', 'groupe_sigle', 'parti_ratt_financier'])) {
         if (deputy_obj[colname].normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase().includes(search_string))
           return true;
       }
       return false;
-    },
-
-    deputyDetailsPreprocessing() {
-      let urls = {}
-      for (let site of this.deputy_details.sites_web) {
-        if (site.site.includes("twitter.com"))
-          urls.twitter = site.site;
-        else if (site.site.includes("facebook.com"))
-          urls.facebook = site.site;
-        else if (!urls.personnal_website)
-          urls.personnal_website = site.site;
-      }
-      for (let email of this.deputy_details.emails)
-        if (email.email.includes("assemblee-nationale.fr"))
-          urls.email = email.email;
-      this.deputy_details.urls = urls;
     },
 
     showDeputyTag(index) {
@@ -179,10 +117,6 @@ export default {
 
     hideDeputyTag() {
       this.show_deputy_tag = false;
-    },
-
-    showLegalNotice() {
-      this.state = 'legal_notice';
     },
   },
 
@@ -202,13 +136,19 @@ export default {
 <style>
 #results-list {
   width: fit-content;
-  max-height: 700px;
-  overflow: scroll;
+  margin: auto;
   margin: auto;
   margin-top: 30px;
+}
+
+.results-found {
+  width: fit-content;
+  max-height: 700px;
+  overflow: scroll;
   scrollbar-color: black white;
   overflow-x: visible;
 }
+
 
 table {
   border-collapse: collapse;
@@ -241,7 +181,6 @@ tr:hover {
 
 .deputy-row:hover {
   background-color: lightskyblue;
-  cursor: pointer;
 }
 
 .deputy-tag {
